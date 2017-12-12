06c20ac6574bd024b4b95333f935834d423f8a64
Hawoly Ba <hawolyba@gmail.com>
2017-12-12 23:04:30 +0100

    Structure principale

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* $(document).ready(function() {
+	var img = $('#grid-one').css("background-image");
+	img = "https://upload.wikimedia.org/wikipedia/commons/5/51/Small_Red_Rose.JPG?width=570"
+    console.log(img);
+
+
+	
+	RGBaster.colors(img, {
+  success: function(payload) {
+    // You now have the payload.
+    console.log(payload.dominant);
+    $('.bloc-text').css("background-color", payload.dominant)
+  }
+});
+
+
+
+}); */
+
